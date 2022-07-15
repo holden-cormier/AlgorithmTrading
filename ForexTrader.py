9b48482f8dce7e77cd06065c5aeb7743c1f5c737
Holden Cormier <holdencormier@gmail.com>
2022-07-15 19:48:21 -0400

    first deep learning model algo

--- ForexTrader.py
+++ ForexTrader.py
@@ -4,10 +4,11 @@ import tpqoa
 from datetime import datetime, timedelta
 import time
 import pickle
+import keras
 
 
 class ForexTrader(tpqoa.tpqoa):
-    def __init__(self, conf_file, instrument, bar_length, lags, model, units):
+    def __init__(self, conf_file, instrument, bar_length, lags, model, units, window, mu, std):
         super().__init__(conf_file)
         self.instrument = instrument
         self.bar_length = pd.to_timedelta(bar_length)
@@ -21,6 +22,9 @@ class ForexTrader(tpqoa.tpqoa):
 
         self.lags = lags
         self.model = model
+        self.window = window
+        self.mu = mu
+        self.std = std
 
     def get_most_recent(self, days=5):
         while True:
@@ -38,12 +42,16 @@ class ForexTrader(tpqoa.tpqoa):
                 break
 
     def on_success(self, time, bid, ask):
-        print(self.ticks, end=" ")
+        if self.ticks % 1000 == 0:
+            print(self.ticks, end=" ")
+        if self.ticks == 1:
+            print("Trader is running")
 
         recent_tick = pd.to_datetime(time)
         df = pd.DataFrame({self.instrument: (ask + bid) / 2},
                           index=[recent_tick])
-        self.tick_data = self.tick_data.append(df)
+  #      self.tick_data = self.tick_data.append(df)
+        self.tick_data = pd.concat([self.tick_data, df])
 
         if recent_tick - self.last_bar > self.bar_length:
             self.resample_and_join()
@@ -52,6 +60,7 @@ class ForexTrader(tpqoa.tpqoa):
 
     def resample_and_join(self):
         self.raw_data = self.raw_data.append(self.tick_data.resample(self.bar_length, label="right").last().ffill().iloc[:-1])
+        # self.raw_data = pd.concat(self.raw_data, self.tick_data.resample(self.bar_length, label="right").last().ffill().iloc[:-1])
         self.tick_data = self.tick_data.iloc[-1:]
         self.last_bar = self.raw_data.index[-1]
 
@@ -61,13 +70,39 @@ class ForexTrader(tpqoa.tpqoa):
         # ml strategy
         df = df.append(self.tick_data)
         df["returns"] = np.log(df[self.instrument] / df[self.instrument].shift())
-        cols = []
-        for lag in range(1, self.lags + 1):
-            col = "lag{}".format(lag)
-            df[col] = df.returns.shift(lag)
-            cols.append(col)
+        df["dir"] = np.where(df["returns"] > 0, 1, 0)
+        df["sma"] = df[self.instrument].rolling(self.window).mean() - df[self.instrument].rolling(150).mean()
+        df["boll"] = (df[self.instrument] - df[self.instrument].rolling(self.window).mean()) / df[
+            self.instrument].rolling(self.window).std()
+        df["min"] = df[self.instrument].rolling(self.window).min() / df[self.instrument] - 1
+        df["max"] = df[self.instrument].rolling(self.window).max() / df[self.instrument] - 1
+        df["mom"] = df["returns"].rolling(3).mean()
+        df["vol"] = df["returns"].rolling(self.window).std()
         df.dropna(inplace=True)
-        df["position"] = lm.predict(df[cols])
+
+        # create lags
+        self.cols = []
+        features = ["dir", "sma", "boll", "min", "max", "mom", "vol"]
+
+        for f in features:
+            for lag in range(1, self.lags + 1):
+                col = "{}_lag_{}".format(f, lag)
+                df[col] = df[f].shift(lag)
+                self.cols.append(col)
+        df.dropna(inplace=True)
+
+        # standardization
+        df_s = (df - self.mu) / self.std
+        # predict
+        df["proba"] = self.model.predict(df_s[self.cols])
+
+        # determine positions
+        df = df.loc[self.start_time:].copy()
+        df["position"] = np.where(df.proba < 0.47, -1, np.nan)
+        df["position"] = np.where(df.proba > 0.53, 1, df.position)
+        df["position"] = df.position.ffill().fillna(0)
+
+        self.data = df.copy()
 
         self.data = df.copy()
 
@@ -111,12 +146,24 @@ class ForexTrader(tpqoa.tpqoa):
 
 
 if __name__ == "__main__":
-    lm = pickle.load(open("logreg.pkl", "rb"))
-    trader = ForexTrader("oanda.cfg", "EUR_USD", "5min", lags=4, model=lm, units=100000)
+    # lm = pickle.load(open("logreg.pkl", "rb"))
+    # trader = ForexTrader("oanda.cfg", "EUR_USD", "5min", lags=4, model=lm, units=100000)
+    # trader.get_most_recent()
+    # trader.stream_data(trader.instrument, stop=50000)
+    # if trader.position != 0:
+    #     close_order = trader.create_order(trader.instrument, units=-trader.position * trader.units,
+    #                                       suppress=True, ret=True)
+    #     trader.report_trade(close_order, "CLOSING ALL POSITIONS")
+    #     trader.position = 0
+    model = keras.models.load_model("DNN_model")
+    params = pickle.load(open("params.pkl", "rb"))
+    mu = params["mu"]
+    std = params["std"]
+    trader = ForexTrader("oanda.cfg", "EUR_USD", bar_length="20min", lags=5, model=model, units=100000, window=50, mu=mu, std=std)
     trader.get_most_recent()
-    trader.stream_data(trader.instrument, stop=500)
+    trader.stream_data(trader.instrument, stop=1000)
     if trader.position != 0:
-        close_order = trader.create_order(trader.instrument, units=-trader.position * trader.units,
-                                          suppress=True, ret=True)
-        trader.report_trade(close_order, "GOING NEUTRAL")
+        close_order = trader.create_order(trader.instrument, units=-trader.position * trader.units, suppress=True, ret=True)
+        trader.report_trade(close_order, "Closing all positions")
         trader.position = 0
+
--- MachineLearning.py
+++ MachineLearning.py
@@ -2,21 +2,103 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 import pickle
+import matplotlib as plt
+from DNNModel import *
 
 if __name__ == "__main__":
-    # creating and saving ml model
-    data = pd.read_csv("five_minute.csv", parse_dates=["time"], index_col="time")
-    data["returns"] = np.log(data.div(data.shift(1)))
-    data.dropna(inplace=True)
-    data["direction"] = np.sign(data.returns)
-    lags = 4
+    # # creating and saving ml model
+    # data = pd.read_csv("five_minute.csv", parse_dates=["time"], index_col="time")
+    # data["returns"] = np.log(data.div(data.shift(1)))
+    # data.dropna(inplace=True)
+    # data["direction"] = np.sign(data.returns)
+    # lags = 4
+    # cols = []
+    # for lag in range(1, lags + 1):
+    #     col = "lag{}".format(lag)
+    #     data[col] = data.returns.shift(lag)
+    #     cols.append(col)
+    # data.dropna(inplace=True)
+    # lm = LogisticRegression(C=1e6, max_iter=100000, multi_class="ovr")
+    # lm.fit(data[cols], data.direction)
+    # pickle.dump(lm, open("logreg.pkl", "wb"))
+    # exit(0)
+    file_data = pd.read_csv("DNN_data.csv", parse_dates=["time"], index_col="time")
+    instrument = file_data.columns[0]
+    file_data["returns"] = np.log(file_data[instrument] / file_data[instrument].shift())
+
+    # creating features to determine position
+    df = file_data.copy()
+    df["dir"] = np.where(df["returns"] > 0, 1, 0)
+    # simple moving average feature
+    min_window = 50
+    max_window = 150
+    df["sma"] = df[instrument].rolling(min_window).mean() - df[instrument].rolling(max_window).mean()
+    # bollinger bands feature
+    df["boll"] = (df[instrument] - df[instrument].rolling(min_window).mean()) / (df[instrument].rolling(min_window).std())
+    # rolling min vs current feature
+    df["min"] = df[instrument].rolling(min_window).min() / df[instrument] - 1
+    # rolling max vs current feature
+    df["max"] = df[instrument].rolling(min_window).max() / df[instrument] - 1
+    # momentum based feature
+    df["mom"] = df["returns"].rolling(3).mean()
+    # volitility based feature
+    df["vol"] = df["returns"].rolling(min_window).std()
+
+    df.dropna(inplace=True)
+
+    # adding lags
+    lags = 5
     cols = []
-    for lag in range(1, lags + 1):
-        col = "lag{}".format(lag)
-        data[col] = data.returns.shift(lag)
-        cols.append(col)
-    data.dropna(inplace=True)
-    lm = LogisticRegression(C=1e6, max_iter=100000, multi_class="ovr")
-    lm.fit(data[cols], data.direction)
-    pickle.dump(lm, open("logreg.pkl", "wb"))
-    exit(0)
+    features = ["dir", "sma", "boll", "min", "max", "mom", "vol"]
+    for feature in features:
+        for lag in range(1, lags + 1):
+            col = "{}_lag_{}".format(feature, lag)
+            df[col] = df[feature].shift(lag)
+            cols.append(col)
+    df.dropna(inplace=True)
+
+    # creating training and test data sets
+    div = int(len(df) * 0.8)
+    train = df.iloc[:div].copy()
+    test = df.iloc[div:].copy()
+    mu = train.mean()
+    std = train.std()
+    # standardize training set
+    train_s = (train - mu) / std
+    set_seeds(100)
+    model = create_model(hl=3, hu=50, dropout=True, input_dim=len(cols))
+    model.fit(x=train_s[cols], y=train["dir"], epochs=50, verbose=False, validation_split=0.2, shuffle=False, class_weight=cw(train))
+
+    model.save("DNN_model")
+    params = {"mu": mu, "std": std}
+    pickle.dump(params, open("params.pkl", "wb"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
